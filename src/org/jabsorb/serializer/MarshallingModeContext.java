63dca7326dfdb4f4f78af19c7f8c473212f474a0
singhrohit23 <rohitsingh@arista.com>
2026-06-15 16:41:04 +0530

    NGFW-15826: fix InheritableThreadLocal race in MarshallingModeContext
    Override childValue() to return a defensive copy of the parent's
    ArrayDeque. Child threads sharing the parent's mutable stack causes
    concurrent push/pop corruption on JDK21 during UVM startup.

--- src/org/jabsorb/serializer/MarshallingModeContext.java
+++ src/org/jabsorb/serializer/MarshallingModeContext.java
@@ -1,8 +1,7 @@
 package org.jabsorb.serializer;
 
-import java.util.Deque;
-import java.util.Stack;
 import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * Context that holds the marshalling mode for a given thread.
@@ -15,6 +14,10 @@ public class MarshallingModeContext {
         @Override
         protected Deque<MarshallingMode> initialValue() {
             return new ArrayDeque<>();
+        }
+        @Override
+        protected Deque<MarshallingMode> childValue(Deque<MarshallingMode> parentValue) {
+            return new ArrayDeque<>(parentValue);
         }
     };
 
